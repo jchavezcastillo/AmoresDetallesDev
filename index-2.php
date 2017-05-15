a75cc319aa2a92241e7746e17a9015e3aa71c752
jhonatanchavez <jhonatanchavezcastillo@gmail.com>
2017-05-15 14:09:53 -0500

    Nuevo home
    Se agrega un index alternativo para experimentar con la maquetación del
    home

---
+++ index-2.php
@@ -0,0 +1,72 @@
+
+<!--MAQUETACIÓN http://getbootstrap.com/components/-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="lib/jquery-1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <style type="text/css">
+      .redondeo
+      {
+        webkit-border-radius: 0px 0px 58px 58px;
+        -moz-border-radius: 0px 0px 58px 58px;
+        border-radius: 0px 0px 58px 58px;
+
+        moz-box-shadow: 0px 0px 7px #000000;
+        -webkit-box-shadow: 0px 0px 7px #000000;
+        box-shadow: 0px 0px 7px #000000;
+      }
+      .blue
+      {
+        background: #222261;
+      }
+      .white
+      {
+        background: #ffffff;
+      }
+      .cortinilla > div
+      {
+        float: left;
+        height: 150px;
+       
+      }
+    </style>
+
+    <div class="row center-block cortinilla">
+      <div class="col-md-1 col-md-push-1 col-sm-1 blue redondeo"></div>
+      <div class="col-md-1 col-md-pull-1 col-sm-1 white redondeo"></div>
+      <div class="col-md-1 col-md-push-1 col-sm-1 blue redondeo"></div>
+      <div class="col-md-1 col-md-pull-1 col-sm-1 white redondeo"></div>
+      <div class="col-md-1 col-md-push-1 col-sm-1 blue redondeo"></div>
+      <div class="col-md-1 col-md-pull-1 col-sm-1 white redondeo"></div>
+      <div class="col-md-1 col-md-push-1 col-sm-1 blue redondeo"></div>
+      <div class="col-md-1 col-md-pull-1 col-sm-1 white redondeo"></div>
+      <div class="col-md-1 col-md-push-1 col-sm-1 blue redondeo"></div>
+      <div class="col-md-1 col-md-pull-1 col-sm-1 white redondeo"></div>
+      <div class="col-md-1 col-md-push-1 col-sm-1 blue redondeo"></div>
+      <div class="col-md-1 col-md-pull-1 col-sm-1 white redondeo"></div>
+    </div>
+
+  </body>
+</html>
